b15711c85dab1acb70c34ddd74af958bb0bb3247
Amina Haq <haq.3@wright.edu>
2021-03-18 15:47:13 -0400

    Revert "Completed T014 for US005 & US013"

--- PDFEditor/src/ImageExtraction.java
+++ PDFEditor/src/ImageExtraction.java
@@ -1,16 +1,3 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-
-import java.io.File;
-import java.io.IOException;
-
 public class ImageExtraction {
-    public static void main(String args[]) throws IOException {
-
-        //create an empty document object
-        PDDocument document = null;
-
-        //open PDFFile, from hardcoded path
-        document = PDDocument.load( new File("C:/Users/wsucatslabs/Documents/PDFTest/sample.pdf") );
-    }
+    //This is a test
 }
