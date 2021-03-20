d4f0449711ea1bc71258a0e534e8bc239d4e4011
hazejjk <54415453+hazejjk@users.noreply.github.com>
2021-03-20 14:30:46 -0400

    Started on t022 - Created searchString.java

---
+++ PDFEditor/src/pdfBoxString/searchString.java
@@ -0,0 +1,36 @@
+package pdfBoxString;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class searchString {
+    public static void main(String[] args) throws IOException {
+        String str1 = null;
+        String strSearch = "Sample_string_here";
+
+        /*
+                use a try and catch here
+                try:    open sample document
+                        parse document
+                        use pdfStripper to get text from document -> str1
+                        close sample document
+                catch:  file not found
+         */
+
+
+        // TEST TEST TEST
+        // Use a scanner
+        Scanner scan = new Scanner(str1);
+
+        int num = 0;    // Counter
+        // While loop through each line
+        while (scan.hasNext()) {
+            String result = scan.nextLine();
+            // If str1 contains strSearch, count how many times it is found
+            if (result.contains(strSearch)) {
+                num = num + 1;
+            }
+        }
+        System.out.println("" + num);
+    }
+}
