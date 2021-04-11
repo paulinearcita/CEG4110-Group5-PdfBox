fb6f0a0b8a949fc9e45937967ef96324107c314e
sanyam-goyal <sanyam.goyal.mail@gmail.com>
2021-04-10 22:50:05 -0400

    Made changes in the code to make it more user friendly.

--- PDFEditor/src/addImage.java
+++ PDFEditor/src/addImage.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.Scanner;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -8,7 +9,7 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 public class addImage {
     public static void main(String args[]) throws Exception {
 
-        ///Loading an existing document
+        //Loading an existing document
         File file = new File("C://temp//sample_form.pdf");
         PDDocument doc = PDDocument.load(file);
 
@@ -20,10 +21,27 @@ public class addImage {
         System.out.println();
 
         System.out.print("Enter the Page Number where you want Image in PDF ");
+        System.out.println();
 
         ///Scanner object to get the input from the console
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
+
+        System.out.print("Enter the x coordinate position where you want Image in PDF ");
+        System.out.println();
+        int x = in.nextInt();
+
+        System.out.print("Enter the y coordinate position where you want Image in PDF ");
+        System.out.println();
+        int y = in.nextInt();
+
+        System.out.print("Enter the width of the image ");
+        System.out.println();
+        int width = in.nextInt();
+
+        System.out.print("Enter the width of the image ");
+        System.out.println();
+        int height = in.nextInt();
 
         if  (a == 0) {
             System.out.print("Error: You cannot add Image on page 0. Please enter a number > 0 ");
@@ -34,21 +52,22 @@ public class addImage {
         }
         ///Add the Image to the page number specified by the user
         else  {
-            ///Retrieving the page
+            //Retrieving the page
             PDPage page = doc.getPage(a-1);
-           ///Creating PDImageXObject object
+            //Creating PDImageXObject object
             PDImageXObject pdImage = PDImageXObject.createFromFile("C://temp//image.png",doc);
-            ///creating the PDPageContentStream object
+            //creating the PDPageContentStream object
             PDPageContentStream contents = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true);
-            ///Drawing the image in the PDF document
-            contents.drawXObject( pdImage, 10, 20, 100, 100 );
+            //Drawing the image in the PDF document
+            contents.drawXObject( pdImage,x,y,width,height);
             System.out.println("Image inserted");
-            ///Closing the PDPageContentStream object
+            //Closing the PDPageContentStream object
             contents.close();
         }
-        ///Saving the document
+        //Saving the document
         doc.save(new File("C://temp//sample_form.pdf"));
-       ///Closing the document
+
+        //Closing the document
         doc.close();
     }
 }
