379e0883741cc74f6c08f97b96d9cd025da7ccb5
Amina Haq <haq.3@wright.edu>
2021-04-23 00:14:52 -0400

    Update DocumentCreation.java

--- PDFEditor/src/DocumentCreation.java
+++ PDFEditor/src/DocumentCreation.java
@@ -3,13 +3,15 @@ import org.apache.pdfbox.pdmodel.PDPage;
 
 import java.io.IOException;
 
+///Creates empty PDF with a single page.
+///creates PDF at a apecified path.
 public class DocumentCreation {
-    
+   ///Main method created for preliminary testing
    public static void main (String args[]) throws IOException {
        
       //Creating PDF document object 
-      PDDocument document = new PDDocument();
-      PDPage my_page = new PDPage();
+      PDDocument document = new PDDocument(); ///< A PDF document variable
+      PDPage my_page = new PDPage(); ///< A PDF page variable
       document.addPage(my_page);
        
       //Saving the document
